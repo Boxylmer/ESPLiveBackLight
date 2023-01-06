70ba78917783a95e64074610de5fc6b4ef7e2810
Will <sypheren@gmail.com>
2023-01-06 02:10:07 -0600

    grid system works

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ import mss
 import mss.tools
 sct = mss.mss()
 
-# monitor = sct.monitors[2]
-
-# TOP = (monitor["left"], monitor["top"], 0,monitor["height"])
-# scr_top = sct.grab(TOP)
+# monitor = sct.monitors[1]
+# scr_top = sct.grab((monitor["left"], monitor["top"], monitor["left"] + monitor["width"], monitor["top"] + monitor["height"]))
 # img = Image.frombytes("RGB", scr_top.size, scr_top.bgra, "raw", "BGRX")
 # img.show()
 
@@ -45,7 +43,9 @@ class MonitorOrchestrator:
     
     def __init__(self, monitor_borders):
         self.monitor_borders = monitor_borders
-    
+        self.monitor_ids = [b.monitor_id for b in monitor_borders]
+        print(self.monitor_ids)
+        self.grid_positions = self._find_monitor_grid_positions()
     # def _get_monitor_xy_from_id(self, id):
     #     return sct.monitors[id]["left"], sct.monitors[id]["top"]
 
@@ -69,19 +69,60 @@ class MonitorOrchestrator:
     def _first_monitor_idx(self):
         target = bottom_left_corner(0)  # farthest bottom left of the virtual monitor
         best_distance = self._dist(target, top_right_corner(0))  # naieve initial target that, by definition, must be higher than any corner
-        print(best_distance)
+
         best_candidate_idx = 0
         for idx, monitor_border in enumerate(self.monitor_borders):
             candidate_distance = self._dist(target, self._bottom_left_corner(monitor_border))
-            print(candidate_distance)
             if candidate_distance < best_distance:
                 best_candidate_idx = idx
                 best_distance = candidate_distance
+        # print(best_candidate_idx)
         return best_candidate_idx
 
         
-    def _find_monitor_positions(self):
-        pass
+    def _find_monitor_grid_positions(self):
+        naive_first_position_idx = self._first_monitor_idx()
+        naive_first_id = self.monitor_borders[naive_first_position_idx].monitor_id
+        naive_first_position = (0, 0)
+        remaining_position_ids = self.monitor_ids.copy()
+        remaining_position_ids.remove(naive_first_id)
+        naive_ids_to_position = {naive_first_id: naive_first_position}
+
+        dirs = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+        print("all positions:")
+        for pid in self.monitor_ids:
+            print("   ", pid, ": ", top_left_corner(pid))
+        while len(remaining_position_ids) > 0: 
+            found_one_during_loop = False  # sets to true if a monitor was in a non-grid position
+            for pid in remaining_position_ids:
+                print("Looking for location of monitor: ", pid)
+                if found_one_during_loop: break
+                for d in dirs:
+                    print("-Looking in direction: ", d)
+                    if found_one_during_loop: break
+                    original_position = top_left_corner(pid)
+                    possible_known_position = original_position[0] - d[0] * sct.monitors[pid]["width"], original_position[1] - d[1] * sct.monitors[pid]["height"]
+
+                    for pid_known in naive_ids_to_position.copy().keys():
+                        print("--currently known positions: ", naive_ids_to_position)
+                        print("--possibly known position: ", possible_known_position)
+                        if found_one_during_loop: break
+                        known_position = top_left_corner(pid_known)
+                        print("--comparing to known position: ", known_position)
+                        if self._dist(possible_known_position, known_position) < MonitorOrchestrator.PX_TOLERANCE:
+                            location = naive_ids_to_position[pid_known][0] + d[0], naive_ids_to_position[pid_known][1] + d[1]
+                            naive_ids_to_position[pid] = location
+                            print("---FOUND POSITION: monitor ", pid, " at location ", location)
+                            remaining_position_ids.remove(pid)
+                            found_one_during_loop = True
+                            # raise(Exception("---"))
+            if not found_one_during_loop:
+                raise(Exception("One of the montiors was out of tolerance to be fit into a cardinal grid system!"))
+
+
+        print(naive_ids_to_position)
+
+            
 
     def _generate_monitor_side_path(self):
         pass
@@ -210,107 +251,114 @@ class CanvasGrid:
         self.canvas.coords(self.monitor_text, self.canvas.winfo_width()/2, self.canvas.winfo_height()/2)
 
 
-#### make tkinter gui
-window = tk.Tk()
-window.title("Boxman Fiddlejig")
-# ipframe = tk.Frame(window)
-# ipentry = tk.Entry(ipframe)
-# ipentry.grid(row=0, column=1)
-# urllabel = tk.Label(ipframe, text="Device URL:")
-# urllabel.grid(row=0, column=0)
-# ipframe.pack()
-canvases = []  # we might not need to save these just yet
 mbps = []
-grids = []
-monitor_active_buttons = []
-widths_input_fields = []
-heights_input_fields = []
-
-for monitor_id in find_monitor_ids():
-    frame = tk.Frame(window)
-    # sframe = tk.Frame(frame)
-    mframe = tk.Frame(frame)
-    # sframe.pack()
-    mframe.pack(fill="both", expand=True)
-
-    # t = tk.Label(sframe,text="Active", font=('Helvetica 12 bold'))
-    # t.grid(row=0, column=0, columnspan=3)
-    # monitor_active_buttons.append(tk.Checkbutton(sframe))
-    # monitor_active_buttons[-1].grid(row=0, column=4)
-
-    canvases.append(tk.Canvas(mframe))
-    canvases[-1].pack(fill="both", expand=True)
+monitor_ids = find_monitor_ids()
+for i, monitor_id in enumerate(monitor_ids):
     mbps.append(MonitorBorderPixels(20, 40, monitor_id))
+orchestrator = MonitorOrchestrator(mbps)
+
+
+# #### make tkinter gui
+# window = tk.Tk()
+# window.title("Boxman Fiddlejig")
+# # ipframe = tk.Frame(window)
+# # ipentry = tk.Entry(ipframe)
+# # ipentry.grid(row=0, column=1)
+# # urllabel = tk.Label(ipframe, text="Device URL:")
+# # urllabel.grid(row=0, column=0)
+# # ipframe.pack()
+# canvases = []  # we might not need to save these just yet
+
+# grids = []
+# monitor_active_buttons = []
+# widths_input_fields = []
+# heights_input_fields = []
+
+# for i, monitor_id in enumerate(monitor_ids):
+#     frame = tk.Frame(window)
+#     # sframe = tk.Frame(frame)
+#     mframe = tk.Frame(frame)
+#     # sframe.pack()
+#     mframe.pack(fill="both", expand=True)
+
+#     # t = tk.Label(sframe,text="Active", font=('Helvetica 12 bold'))
+#     # t.grid(row=0, column=0, columnspan=3)
+#     # monitor_active_buttons.append(tk.Checkbutton(sframe))
+#     # monitor_active_buttons[-1].grid(row=0, column=4)
+
+#     canvases.append(tk.Canvas(mframe))
+#     canvases[i].pack(fill="both", expand=True)
     
-    grids.append(CanvasGrid(canvases[-1], mbps[-1]))
+    
+#     grids.append(CanvasGrid(canvases[i], mbps[i]))
 
-    frame.pack(fill="both", expand=True, anchor=tk.S)
+#     frame.pack(fill="both", expand=True, anchor=tk.S)
     
 
-# load the save file and set default values
+# # load the save file and set default values
 
 
-# Set the size of the window
-window.geometry("700x350")
+# # Set the size of the window
+# window.geometry("700x350")
 
 
-# Define a function for quit the window
-def quit_window(icon, item):
-    global SOFTKILL_MODEL
-    SOFTKILL_MODEL = True
-    icon.stop()
-    window.destroy()
+# # Define a function for quit the window
+# def quit_window(icon, item):
+#     global SOFTKILL_MODEL
+#     SOFTKILL_MODEL = True
+#     icon.stop()
+#     window.destroy()
 
-# Define a function to show the window again
-def show_window(icon, item):
-   icon.stop()
-   window.after(0,window.deiconify())
+# # Define a function to show the window again
+# def show_window(icon, item):
+#    icon.stop()
+#    window.after(0,window.deiconify())
 
-# Hide the window and show on the system taskbar
-def hide_window():
-   window.withdraw()
-   image=Image.open("think.ico")
-   menu=(
-    item('Quit', quit_window), 
-    item('Show', show_window, default=True),)
-   icon=pystray.Icon("name", image, "My System Tray Icon", menu)
-   icon.run()
+# # Hide the window and show on the system taskbar
+# def hide_window():
+#    window.withdraw()
+#    image=Image.open("think.ico")
+#    menu=(
+#     item('Quit', quit_window), 
+#     item('Show', show_window, default=True),)
+#    icon=pystray.Icon("name", image, "My System Tray Icon", menu)
+#    icon.run()
 
-window.protocol('WM_DELETE_WINDOW', hide_window)   # uncomment to reactivate tray behavior
+# window.protocol('WM_DELETE_WINDOW', hide_window)   # uncomment to reactivate tray behavior
 
-SOFTKILL_MODEL = False
-def ping_model():
-    global SOFTKILL_MODEL
-    last_refresh_time = time.time()
-    while True:
-        if SOFTKILL_MODEL:
-            SOFTKILL_MODEL = False
-            return
-        if (time.time() - last_refresh_time) * 1000 > REFRESH_TIME_MS:
-            last_refresh_time = time.time()
-            for mbpv in mbps:
-                try:
-                    mbpv.update()
-                except:
-                    print("WARNING: Model loop failed to ping.")
+# SOFTKILL_MODEL = False
+# def ping_model():
+#     global SOFTKILL_MODEL
+#     last_refresh_time = time.time()
+#     while True:
+#         if SOFTKILL_MODEL:
+#             SOFTKILL_MODEL = False
+#             return
+#         if (time.time() - last_refresh_time) * 1000 > REFRESH_TIME_MS:
+#             last_refresh_time = time.time()
+#             for mbpv in mbps:
+#                 try:
+#                     mbpv.update()
+#                 except:
+#                     print("WARNING: Model loop failed to ping.")
         
-        remaining_time = max(0, REFRESH_TIME_MS/1000 - (time.time() - last_refresh_time))
-        print(remaining_time)
-        time.sleep(remaining_time)
+#         remaining_time = max(0, REFRESH_TIME_MS/1000 - (time.time() - last_refresh_time))
+#         print(remaining_time)
+#         time.sleep(remaining_time)
 
-threading.Thread(target=ping_model).start()
+# threading.Thread(target=ping_model).start()
 
-last_refresh_time = time.time()
-while True:
-    if (time.time() - last_refresh_time) * 1000 > GUI_POLLING_TIME_MS:
-        last_refresh_time = time.time()
-        for grid in grids:
-            grid.update()
+# last_refresh_time = time.time()
+# while True:
+#     if (time.time() - last_refresh_time) * 1000 > GUI_POLLING_TIME_MS:
+#         last_refresh_time = time.time()
+#         for grid in grids:
+#             grid.update()
 
-    window.update_idletasks()
-    window.update()
-    remaining_time = max(0, GUI_POLLING_TIME_MS/1000 - (time.time() - last_refresh_time))
-    # print(GUI_POLLING_TIME_MS/1000 - (time.time() - last_refresh_time))
+#     window.update_idletasks()
+#     window.update()
+#     remaining_time = max(0, GUI_POLLING_TIME_MS/1000 - (time.time() - last_refresh_time))
+#     # print(GUI_POLLING_TIME_MS/1000 - (time.time() - last_refresh_time))
 
-    time.sleep(remaining_time)
+#     time.sleep(remaining_time)
 
