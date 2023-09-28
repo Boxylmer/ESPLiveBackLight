3c58fa622620ce63a045b6233c80a890e0ea8719
Will <sypheren@gmail.com>
2023-09-28 18:21:31 -0500

    Rename + made compiler callable from root

--- DesktopApp/compile.bat
+++ DesktopApp/compile.bat
@@ -1,3 +1,4 @@
+pushd "DesktopApp" 
 call ..\.venv\Scripts\activate # expects .venv is in git repo root folder
 pyinstaller --noconfirm ^
             --noconsole ^
@@ -7,3 +8,4 @@ pyinstaller --noconfirm ^
             --icon=Application/resources/think.ico ^
             --name "Boxman Fiddlejig" ^
             compile_target.py
+popd 
