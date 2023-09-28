c401529d466a165a547430a53b3c2e8c79865689
Will <sypheren@gmail.com>
2023-09-28 18:00:28 -0500

    multiline compile

--- Desktop Interface/compile.bat
+++ Desktop Interface/compile.bat
@@ -1,2 +1,9 @@
 call ..\.venv\Scripts\activate # expects .venv is in git repo root folder
-pyinstaller --noconfirm --noconsole --onedir --add-data="Application/resources;Application/resources" --icon=Application/resources/think.ico  compile_target.py
+pyinstaller --noconfirm ^
+            --noconsole ^
+            --onedir ^
+            --contents-directory giblets ^
+            --add-data="Application/resources;Application/resources" ^
+            --icon=Application/resources/think.ico ^
+            --name "Boxman Fiddlejig" ^
+            compile_target.py
